819927e4d859de58de363f6ed81342a7d6e9b989
clmc <zcray@icloud.com>
2014-01-31 20:56:18 -0500

    combat module

--- combat.py
+++ combat.py
@@ -5,11 +5,11 @@ main_loop = True
 # is better than '1' or '0' .
 print "Welcome, adventurer.\n"
 
-def combat_round(ch_l, ch_n, enem_l, enem_n):
+def combat_round(ch_l, ch_n, ch_d, enem_l, enem_n, enem_d):
  enem_mx = enem_l
 
  print "An enemy monster appears!"
- print "The", EN_nm, "hisses.\n "
+ print "The", enem_n, "hisses.\n "
  while (enem_l > 0):
      # Init Main Combat Menu #
   print "[A]ttack, [R]un, [S]uicide\n What will %s do?" % ch_n
@@ -23,10 +23,10 @@ def combat_round(ch_l, ch_n, enem_l, enem_n):
    atk_input = raw_input("> ")
    if atk_input.lower() == "m":
     print "You slash the", enem_n, "with your weapon."
-    enem_ll = enem_l - ch_dm
+    enem_ll = enem_l - ch_d
     print enem_n, "(%r/%r)" % (enem_ll, enem_mx)
     print "The", enem_n, "takes %r damage, leaving it with %r hp.\
-    (out of %r)" % (ch_dm, enem_ll, enem_l)
+    (out of %r)" % (ch_d, enem_ll, enem_l)
     enem_l = enem_ll
 
    elif atk_input.lower() == "s":
@@ -37,8 +37,8 @@ def combat_round(ch_l, ch_n, enem_l, enem_n):
      print "You cast Fire Blast!"
      sp_dm = 3
      enem_ll = enem_l - sp_dm
-     print EN_nm, "(%r/%r)" % (enem_ll, enem_mx)
-     print "The Fire Blast hits the", EN_nm, ". It takes %r damage, leaving it with %r hp.\
+     print enem_n, "(%r/%r)" % (enem_ll, enem_mx)
+     print "The Fire Blast hits the", enem_n, ". It takes %r damage, leaving it with %r hp.\
      (out of %r)" % (sp_dm, enem_ll, enem_l)
      enem_l = enem_ll
 
@@ -57,14 +57,13 @@ def combat_round(ch_l, ch_n, enem_l, enem_n):
     print "You fire your bow at the %s." % enem_n
     rn_dm = 5
     enem_ll = enem_l - rn_dm
-    print EN_nm, "(%r/%r)" % (enem_ll, enem_mx)
+    print enem_n, "(%r/%r)" % (enem_ll, enem_mx)
     print "You strike the", enem_n, "with your bow. It takes %r damage, leaving it with %r hp.\
     (out of %r)" % (rn_dm, enem_ll, enem_l)
     enem_l = enem_ll
 
    else:
     print "Unrecognized command."
-
 
   elif pl_input.lower() == "r":
    print "R recognized"
@@ -78,6 +77,9 @@ def combat_round(ch_l, ch_n, enem_l, enem_n):
      print "On second thought, you like living."
   else:
    print "Unrecognized command."
+
+def enem_assign_dk(en_hitp, en_dmg, en_name):
+ EN_hp = en_hitp ; EN_nm = en_name ; EN_dm = en_dmg
 
 ## ensure function ended properly by returning 1 
 # return 1;
@@ -114,13 +116,20 @@ enemDmg_3 = 3 ; enemCrit_3 = 7 ; enemHP_3 = 35 ; enemMag_3 = 2
 
 ## Grab variables from DB or test environment and assign them to proper
 #  variables recognized by the combat engine
+#        ## INIT ENGINE BASEVALS ##
 ch_hp = charHP ; ch_dm = charDmg ; ch_cr = charCrit ; ch_mg = charMag
 EN_hp = enemHP_1 ; EN_dm = enemDmg_1 ; EN_cr = enemCrit_1 ; EN_mg = enemMag_1
 ch_nm = charName ; EN_nm = enemName_1
 #
 ##
 
-combat_round(ch_hp, ch_nm, EN_hp, EN_nm)
+# Assign player stats
+#ch_hp = charHP ; ch_nm = charName ; ch_dm = charDmg
+
+# Assign opponent
+enem_assign_dk(enemHP_3, enemDmg_3, enemName_3)
+
+combat_round(ch_hp, ch_nm, ch_dm, EN_hp, EN_nm, EN_dm)
 
 ## 
 # while ch_hp > 0 or EN_hp > 0:
